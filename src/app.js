f3a218e6d30bd95466cc7003c21b6cc5e7b62e8a
Brayan <tronks_465@hotmail.com>
2022-12-03 18:33:41 -0500

    entrega

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 
 const port = 9000
+const app = express()
 
-const app = express
+const usersRouter = require('./users/users.router')
+
+app.use(express.json())
+
+app.use('/api/v1', usersRouter)
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Ok!'})
 }) 
 
-app.listen(() => {
+app.listen(port, () => {
     console.log(`Server started at port ${port}`)
 })
--- src/users/users.services.js
+++ src/users/users.services.js
@@ -1,22 +1,82 @@
+const usersController = require('./users.controllers')
 
-const getAllUsers = () => {
-
+const getAllUsers = async (req, res) => { 
+    /* try {
+        const data = await usersController.findAllUsers()
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    } */
+    usersController.findAllUsers()
+        .then( (data) => {
+            res.status(200).json(data)
+        })
+        .catch( (err) => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-const getUserById = () => {
-    
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    usersController.findUserById(id)
+        .then( (data) => {
+            if(data){
+                res.status(200).json(data)
+            }else{
+                res.status(404).json({ message: 'Invalid ID' })
+            }
+        })
+        .catch( (err) => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-const postUser = () => {
-    
+const postUser = (req, res) => {
+    const { first_name, last_name, email, password, birthday } = req.body
+    usersController.createUser({ first_name, last_name, email, password, birthday })
+        .then( (data) => {
+            res.status(201).json(data)
+        })
+        .catch( (err) => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-const patchUser = () => {
-    
+const patchUser = (req, res) => {
+    const { id } = req.params
+    const { first_name, last_name, email, password, birthday } = req.body
+    usersController.updateUser(id, { first_name, last_name, email, password, birthday })
+        .then( (data) => {
+            if(data){
+                res.status(200).json({message: 'User updated succesfully with id: ' + id})
+            }else{
+                res.status(404).json({ message: 'Invalid ID' })
+            }
+        })
+        .catch( (err) => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-const deleteUser = () => {
-    
+const deleteUser = (req, res) => {
+    const { id } = req.params
+    usersController.deleteUser(id)
+        .then( (data) => {
+            if(data){
+                res.status(200).json({message: 'Deleted succesfully'})
+            }else{
+                res.status(404).json({ message: 'Invalid ID' })
+            }
+        })
+        .catch( (err) => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-
+module.exports = {
+    getAllUsers,
+    getUserById,
+    postUser,
+    patchUser,
+    deleteUser
+}
